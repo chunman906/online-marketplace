60b18066b0d0c36b8ccdbead02b860a5a9e18f5a
chun man poon <chunman906@hotmail.com>
2022-06-18 23:12:32 +0100

    create product.rb

---
+++ lib/product.rb
@@ -0,0 +1,9 @@
+class Product
+  def initialise
+    @price = price
+    @lavender_heart = lavender_heart
+    @personalise_cufflink = personalise_cufflink
+    @kids_t_shirt = kids_t_shirt
+  end
+
+end
